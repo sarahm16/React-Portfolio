1cf21c5241882549cb72f2030492500119f7f1fb
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-08-13 11:23:59 -0700

    created aboutme.js, contact.js, and projects.js. Added router to App.js to navigate between pages

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Aboutme from './pages/aboutme';
+import Contact from './pages/contact';
+import Projects from './pages/projects';
+
+import { Router, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path='/' component={Aboutme} />
+      <Route exact path='/contact' component={Contact} />
+      <Route exact path='/projects' component={Projects} />
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/pages/aboutme.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Aboutme() {
+    return(
+        <h2>About me</h2>
+    )
+}
+export default Aboutme;
