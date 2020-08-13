fce05a3d5426633ae0b3ee7fd0c6778ae2162ab8
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-08-13 15:57:29 -0700

    added input fields for name and phone in contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Navbar from '../components/navbar/navbar';
 
 import './style.css';
 
-function Contact() {
-    return(
-        //make a form to send me email
-        //include input field for their phone and email
-        <div>
-            <Navbar page='contact' />
-            <div className='contact container bg-light'>
-                <div className='row'>
-                    <ul>
-                        <li>Github</li>
-                        <li>Linked In</li>
-                        <li>Phone</li>                    
-                    </ul>
+class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: '',
+            email: '',
+            phone: '',
+            isSubmitted: false
+        }
+    }
+
+    onChange = (event) => {
+        console.log(event.target.id)
+        this.setState({[event.target.id] : event.target.value})
+    }
+
+    render() {
+        return(
+            //make a form to send me email
+            //include input field for their phone and email
+            <div>
+                <Navbar page='contact' />
+                <div className='contact container bg-light'>
+                    <div className='row contact-header'>
+                        <h3>Contact Me</h3>
+                    </div>
+                    <div className='row'>
+                        <form>
+                            <div className="row user-contact">
+                                <div className="col">
+                                    <input type="text" className="form-control" placeholder="Name" />
+                                </div>
+                                <div className="col">
+                                    <input type="text" className="form-control" placeholder="Phone" />
+                                </div>
+                            </div>
+                            <textarea id='message' value={this.state.message} onChange={this.onChange} />
+                        </form>
+                    </div>
                 </div>
             </div>
-
-        </div>
-    )
+        )
+    }
 }
 
 export default Contact;
