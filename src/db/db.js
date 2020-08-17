41ea6c2bc9836e2d5d15a7bed69944090c3207f9
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-08-17 12:40:40 -0700

    added more info about projects to db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,24 @@
 let projectList = [
     {
         name: 'Fair Weather Hiking',
-        description: 'testing',
-        technologies: []
+        description: 'An app designed for hikers to search for hikes based on mileage, distance to trailhead, and elevation gain. Users can save hikes into their favorites, mark complete, and track their month by month stats for elevation gain and distance hiked.',
+        technologies: ['React.js', 'Node.js', 'MongoDB', 'Redux', 'Express.js', 'Moment.js', 'Zipcodes.js', 'Materialize CSS', 'Cloudinary.js'],
+        url: 'fairweatherhiking.herokuapp.com',
+        image: ''
     },
     {
         name: 'Urban Forage',
-        description: 'testing',
-        technologies: []
+        description: 'A social app designed to help friends decide which restaurant to go to within a certain mileage',
+        technologies: ['Handlebars.js', 'MySQL', 'Sequelize.js', 'Express.js', 'Node.js', 'Bootstrap CSS', ],
+        url: 'https://polar-cliffs-12845.herokuapp.com/',
+        image: ''
     },
     {
         name: 'Festival Friends',
         description: 'testing',
-        technologies: []
+        technologies: [],
+        url: '',
+        image: ''
     },
     {
         name: 'FindMyDoctor',
