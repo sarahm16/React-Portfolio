31e5685c7d7f2e410525f791a3bc1026cea155f2
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-13 20:22:25 -0800

    added css styling to success and error alerts in contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -83,12 +83,15 @@ class Contact extends Component {
                                     <button type='submit' id='submit'><i className="far fa-paper-plane"></i></button>
                                 </div>
                             </div>
-                            {this.state.isSubmitted && <div className="alert alert-success" id='success-alert' role="alert">
+                            
+                            {this.state.status === 'SUCCESS' && <div className='status' id='success'>
                                 Thank you! I will return your email shortly
                             </div>}
-                            {/* <div className="alert alert-success" id='success-alert' role="alert">
-                                Thank you! I will contact you shortly
-                            </div> */}
+
+                            {this.state.status === 'ERROR' && <div className='error'>
+                                Oops, something went wrong!
+                            </div>}
+
                         </form>
 
 
